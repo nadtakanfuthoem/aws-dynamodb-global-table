bf3f5c861fff9c6c8cf3ac3a4a4853276c6279df
nadtakanjones <nadtakan.jones@gmail.com>
2022-05-23 22:28:09 -0700

    custom resources

---
+++ kms-stacks/us-west-1/index.js
@@ -0,0 +1,31 @@
+var response = require("cfn-response");
+exports.handler = function (event, context) {
+  console.log("REQUEST RECEIVED:\\n", JSON.stringify(event));
+  if (event.RequestType == "Delete") {
+    response.send(event, context, response.SUCCESS);
+    return;
+  }
+  var stackName = event.ResourceProperties.StackName;
+  var responseData = {};
+  if (stackName) {
+    var aws = require("aws-sdk");
+    aws.config.update({region: 'us-east-1'});
+    var cfn = new aws.CloudFormation();
+    cfn.describeStacks({ StackName: stackName }, function (err, data) {
+      if (err) {
+        responseData = { Error: "DescribeStacks call failed" };
+        console.log(responseData.Error + ":\\n", err);
+        response.send(event, context, response.FAILED, responseData);
+      } else {
+        data.Stacks[0].Outputs.forEach(function (output) {
+          responseData[output.OutputKey] = output.OutputValue;
+        });
+        response.send(event, context, response.SUCCESS, responseData);
+      }
+    });
+  } else {
+    responseData = { Error: "Stack name not specified" };
+    console.log(responseData.Error);
+    response.send(event, context, response.FAILED, responseData);
+  }
+};
